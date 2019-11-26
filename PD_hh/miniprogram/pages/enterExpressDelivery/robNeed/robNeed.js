d421d5499658e53bc9a4d56b7be06aa73628587e
huhao <1559531623@qq.com>
2019-11-26 18:06:18 +0800

    新增部分逻辑

--- PD_hh/miniprogram/pages/enterExpressDelivery/robNeed/robNeed.js
+++ PD_hh/miniprogram/pages/enterExpressDelivery/robNeed/robNeed.js
@@ -1,1 +1,53 @@
-Page({})
+Page({
+  data: {
+    step: 1,
+    counterId: '',
+    openid: '',
+    count: 0,
+    queryResult: '',
+    data:null
+  },
+  onLoad: function (options) {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('order').get({
+      success: res => {
+        this.setData({
+          queryResult: JSON.stringify(res.data, null, 2),
+          count:res.data.length,
+          data: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res.data)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  onQuery: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('order').get({
+      success: res => {
+        this.setData({
+          queryResult: JSON.stringify(res.data, null, 2)
+        })
+        console.log('[数据库] [查询记录] 成功: ', res.data)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  rob:function(){
+
+  },
+})
